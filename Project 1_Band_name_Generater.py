6e039b462575e07f113baec798c9b7d647ded659
MORTALE SWAPNIL <157574425+swapnilMortale@users.noreply.github.com>
2024-10-06 18:27:04 +0530

    Add files via upload

---
+++ Project 1_Band_name_Generater.py
@@ -0,0 +1,7 @@
+print("welcome to the band generater.")
+
+city= input("Which city did you grou up in : \n")
+
+pet = input("Whats the name of a pet ?\n")
+
+print("your band name could be " +city+ " "+pet)
